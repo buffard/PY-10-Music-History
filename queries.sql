30e15506d479d05e279d2cfde54a6eb23820efe6
buffard <samuelwebber19@gmail.com>
2019-01-14 14:51:36 -0600

    boiler plate

---
+++ queries.sql
@@ -0,0 +1,5 @@
+-- 1. Query all of the entries in the Genre table
+SELECT * FROM Genre
+-- 2. Using the INSERT statement, add one of your favorite artists to the Artist table.
+-- 3. Using the INSERT statement, add one, or more, albums by your artist to the Album table.
+-- 4. Using the INSERT statement, add some songs that are on that album to the Song table.
